16256ee69df3961d99a6b03c81d6cc65fac29589
Andrew Gagliotti <andrew.gagliotti@studio.unibo.it>
2021-01-31 22:47:29 +0100

    added nesting1 solutions

---
+++ src/it/unibo/oop/lab/nesting1/SportSocialNetworkUserImpl.java
@@ -0,0 +1,143 @@
+/**
+ * 
+ */
+package it.unibo.oop.lab.nesting1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import it.unibo.oop.lab.socialnetwork.SocialNetworkUserImpl;
+import it.unibo.oop.lab.socialnetwork.User;
+
+/**
+ * 
+ * Represents a social network user along with the sports he/she likes to do or
+ * to follow.
+ * 
+ * 1) Complete the definition of the nested static class Sport, featuring just a
+ * field representing the sport name.
+ * 
+ * 2) Add the unimplemented methods.
+ * 
+ * @param <U>
+ *            specific {@link User} type
+ */
+public class SportSocialNetworkUserImpl<U extends User> extends SocialNetworkUserImpl<U> {
+
+    /**
+     * Static {@link Sport} constant.
+     */
+    public static final Sport SOCCER;
+    /**
+     * Static {@link Sport} constant.
+     */
+    public static final Sport F1;
+    /**
+     * Static {@link Sport} constant.
+     */
+    public static final Sport MOTOGP;
+    /**
+     * Static {@link Sport} constant.
+     */
+    public static final Sport VOLLEY;
+    /**
+     * Static {@link Sport} constant.
+     */
+    public static final Sport BASKET;
+    /**
+     * Static {@link Sport} constant.
+     */
+    public static final Sport BIKE;
+
+    static {
+        SOCCER = new Sport("Soccer");
+        F1 = new Sport("Formula 1 pilot");
+        MOTOGP = new Sport("Moto GP pilot");
+        VOLLEY = new Sport("Volley");
+        BASKET = new Sport("Basket");
+        BIKE = new Sport("Bike");
+    }
+
+    /**
+     * Field meant to keep track of the set of sports followed/done by a user.
+     */
+    private final Set<Sport> sports;
+
+    /**
+     * Builds a new {@link SportSocialNetworkUserImpl}.
+     * 
+     * @param name
+     *            the user firstname
+     * @param surname
+     *            the user lastname
+     * @param user
+     *            alias of the user, i.e. the way a user is identified on an
+     *            application
+     */
+    public SportSocialNetworkUserImpl(final String name, final String surname, final String user) {
+        this(name, surname, user, -1);
+    }
+
+    /**
+     * Builds a new {@link SportSocialNetworkUserImpl}.
+     * 
+     * @param name
+     *            the user firstname
+     * @param surname
+     *            the user lastname
+     * @param userAge
+     *            user's age
+     * @param user
+     *            alias of the user, i.e. the way a user is identified on an
+     *            application
+     */
+    public SportSocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
+        super(name, surname, user, userAge);
+        this.sports = new HashSet<>();
+    }
+
+    /**
+     * Add a new sport followed by this user: if the user already likes or does
+     * the sport, nothing happens.
+     * 
+     * @param sport
+     *            a sport followed/done by the user
+     */
+    public void addSport(final Sport sport) {
+        this.sports.add(sport);
+    }
+
+    /**
+     * Returns true if a user likes/does a given sport.
+     * 
+     * @param s
+     *            sport to use as an input
+     * @return true if a user likes sport s
+     */
+    public boolean hasSport(final Sport s) {
+        return this.sports.contains(s);
+    }
+
+    public static final class Sport {
+
+        private final String name;
+
+        public Sport(final String string) {
+            this.name = string;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            //spotbug mi segnalava che non controllavo un possibile valore null
+            if (o == null) {
+                return false;
+            }
+            return o.equals(this.getClass());
+        }
+
+        @Override
+        public int hashCode() {
+            return this.name.hashCode();
+        }
+    }
+}
